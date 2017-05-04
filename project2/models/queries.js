453567bafdc77e817a140846f484bb69e3dab609
Student <student@GAs-MacBook-Air-3.local>
2017-05-04 17:59:46 -0400

    got api call working

--- project2/models/queries.js
+++ project2/models/queries.js
@@ -1,39 +1,50 @@
-/*
 var promise = require('bluebird');
 var options = { promiseLib: promise };
-
+var axios = require('axios');
 var pgp = require('pg-promise')(options);
 
 var connectionString = 'postgres://localhost:5432/contacts_db';
 var db = pgp(connectionString);
 
+//var url = `http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC`
+
+function apiCall(req, res, next) {
+    console.log('apiCall() is awake');
+    axios.get(`http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC`)
+        .then((res) => {
+            var thenCall = res.data.data.url;
+            console.log(thenCall);
+            return thenCall;
+        })
+};
+
 function createContact(req, res, next) {
-	req.body.age = parseInt(req.body.age);
-	db.none('insert into contacts(first, last, age, sex)' 
-		+ 'values(${first}, ${last}, ${age}, ${sex})', req.body)
-	.then(res.redirect('/'))
+    req.body.age = parseInt(req.body.age);
+    db.none('insert into contacts(first, last, age, sex)' +
+            'values(${first}, ${last}, ${age}, ${sex})', req.body)
+        .then(res.redirect('/'));
 }
 
 function getAllContacts(req, res, next) {
-	db.any('select * from contacts').then(function(info) {
-		res.render('index', { title:'All Contacts', data:info });
-	})
+    db.any('select * from contacts').then(function(info) {
+        res.render('index', { title: 'All Contacts', data: info });
+    })
 }
 
 function removeContact(req, res, next) {
-	let contactID = parseInt(req.params.id);
-	console.log(contactID);
-	db.result('delete from contacts where id = $1', contactID);
+    let contactID = parseInt(req.params.id);
+    console.log(contactID);
+    db.result('delete from contacts where id = $1', contactID);
 }
 
 function updateContact(req, res, next) {
-	db.none('update contacts set first=$1 where id =$2', [req.body.first, parseInt(req.params.id)]);
+    db.none('update contacts set first=$1 where id =$2', [req.body.first, parseInt(req.params.id)]);
 }
 
 module.exports = {
-	createContact: createContact,
-	getAllContacts: getAllContacts,
-	removeContact: removeContact,
-	updateContact: updateContact
+    apiCall: apiCall,
+    createContact: createContact,
+    getAllContacts: getAllContacts,
+    removeContact: removeContact,
+    updateContact: updateContact
 }
-*/
--- project2/routes/index.js
+++ project2/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
-// var db = require('../queries');
-
+var db = require('../models/queries');
+var axios = require('axios');
 
 // Figure out code to have the browser wait for other people after a button click for several pages.
 // >>> This will be for: Index, Entry, Vote & Results.
@@ -13,29 +13,56 @@ var router = express.Router();
 var title = 'GIF Me A Laugh!';
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: title });
+    res.render('index', { title: title });
 });
 
-router.get('/entry', function(req, res, next) {
-	console.log("Did this happen?");
-  res.render('entry', { title: title });
+
+function apiCall(req, response, next) {
+    // console.log('apiCall() is awake');
+    axios.get(`http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC`)
+        .then((res) => {
+            response.locals.gifUrl = res.data.data.url;
+            console.log(response.locals.gifUrl);
+            // res.locals.thenCall = res.data.data.url;
+            // console.log(res.locals.thenCall);
+            return next();
+        }).catch((err) => {
+            console.log(err);
+        })
+};
+
+
+router.get('/entry', apiCall, (req, res, next) => {
+    res.render('entry', {
+            title: title,
+            gifUrl: res.locals.gifUrl
+        })
+        // var thenCall = res.data.data.url;
+        // console.log(thenCall);
+        // return thenCall;
 });
+// console.log("entry alive!");
+
+// }).post((res) => {
+//     db.apiCall();
+
 
 router.get('/vote', function(req, res, next) {
-	console.log("What about this?");
-  res.render('vote', { title: title });
+    console.log("What about this?");
+    res.render('vote', { title: title });
 });
 
 router.get('/results', function(req, res, next) {
-	console.log("And Finally this?");
-  res.render('results', { title: title });
+    console.log("And Finally this?");
+    res.render('results', { title: title });
 });
 
-/*
+
 router.get('/', db.getAllContacts);
 router.post('/', db.createContact);
+router.post('/entry', db.apiCall);
 router.delete('/:id', db.removeContact);
 router.patch('/:id', db.updateContact);
-*/
+
 
 module.exports = router;
